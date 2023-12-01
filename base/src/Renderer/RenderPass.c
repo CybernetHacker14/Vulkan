5171872903f0fecf9dd8d915892ec8066ad9ec3f
Ujjwal Raut <ujjwalraut.14@gmail.com>
2023-12-01 12:16:06 +0530

    [Base] Added Render Pass files (#12)

---
+++ base/src/Renderer/RenderPass.c
@@ -0,0 +1,56 @@
+#include "Renderer/RenderPass.h"
+#include "Renderer/Swapchain.h"
+#include "Renderer/Device.h"
+
+#include <vulkan/vulkan.h>
+#include <stdio.h>
+
+VkRenderPass renderPass;
+
+void createRenderPass() {
+    VkAttachmentDescription colorAttachment;
+    colorAttachment.flags = 0;
+    colorAttachment.format = getSwapchainImageFormat();
+    colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+    colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+    colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+    colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+
+    VkAttachmentReference colorAttachmentRef;
+    colorAttachmentRef.attachment = 0;
+    colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    VkSubpassDescription subpass;
+    subpass.flags = 0;
+    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpass.colorAttachmentCount = 1;
+    subpass.pColorAttachments = &colorAttachmentRef;
+    subpass.pResolveAttachments = NULL;
+    subpass.pDepthStencilAttachment = NULL;
+    subpass.inputAttachmentCount = 0;
+    subpass.pInputAttachments = NULL;
+    subpass.preserveAttachmentCount = 0;
+    subpass.pPreserveAttachments = NULL;
+
+    VkRenderPassCreateInfo renderPassInfo;
+    renderPassInfo.pNext = NULL;
+    renderPassInfo.flags = 0;
+    renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    renderPassInfo.dependencyCount = 0;
+    renderPassInfo.pDependencies = NULL;
+    renderPassInfo.attachmentCount = 1;
+    renderPassInfo.pAttachments = &colorAttachment;
+    renderPassInfo.subpassCount = 1;
+    renderPassInfo.pSubpasses = &subpass;
+
+    if (vkCreateRenderPass(getLogicalDevice(), &renderPassInfo, NULL, &renderPass) != VK_SUCCESS) {
+        printf_s("Failed to create render pass\n");
+    }
+}
+
+void destroyRenderPass() {
+    vkDestroyRenderPass(getLogicalDevice(), renderPass, NULL);
+}
